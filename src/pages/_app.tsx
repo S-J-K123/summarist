5d8a60a87971fa49b32a348fdb4a698c21ac8e4d
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-07-18 22:46:18 +0100

    redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import '@component/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
-import { store } from "@/redux/store"
+import store from "../redux.store"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={store}>
+    <Provider store={store} >
         <Component {...pageProps} />
     </Provider>
   )
