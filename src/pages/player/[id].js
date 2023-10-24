592c08067cc669e891e69404dba97935b167a363
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-10-24 20:05:13 +0100

    sidebar

--- src/pages/player/[id].js
+++ src/pages/player/[id].js
@@ -1,5 +1,3 @@
-// Audio.js
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
@@ -9,11 +7,15 @@ import DisplayTrack from "../../components/DisplayTrack";
 import AudioPlayer from "../../components/AudioPlayer";
 import Input from "../../components/Input";
 
+
+
 const Audio = () => {
   const [audio, setAudio] = useState();
   const [loading, setLoading] = useState(true);
-  const router = useRouter();
-  const { id } = router.query;
+  const router = useRouter(); 
+  const { id, pathname } = router.query;
+
+  console.log(id);
 
   async function getAudio() {
     try {
@@ -32,20 +34,26 @@ const Audio = () => {
     getAudio();
   }, [id]);
 
-  // Condition for adjusting sidebar height
-  const isAudioPlayerPresent = !!audio; // Adjust this based on your actual condition
-  const sidebarHeight = isAudioPlayerPresent ? "200px" : "300px";
-
   return (
     <div>
-      <SideBar style={{ height: sidebarHeight }} />
-      {!loading && audio && (
+      <SideBar
+        className={`sidebar__wrapper ${
+          pathname && pathname.startsWith("/player")
+            ? "sidebar__bump-up"
+            : ""
+        }`}
+      />
+      {!loading && audio ? (
         <>
           <DisplayTrack audio={audio} />
           <AudioPlayer audio={audio} />
           <div className="input-wrapper">
             <Input />
           </div>
+          {/* <div>
+            {audio.title}
+            {audio.summary}
+          </div> */}
           <div className="audio__wrapper">
             <div className="audio__track--wrapper">
               <figure className="audio__track--image-mask">
@@ -59,11 +67,26 @@ const Audio = () => {
             <div className="audio__controls--wrapper">
               <div className="audio__controls">
                 <button className="audio__controls--btn"></button>
+                {/* <button className="audio__controls--btn audio__controls--btn-play">
+                  <PlayArrowIcon />
+                </button> */}
                 <button className="audio__controls--btn"></button>
               </div>
             </div>
+            {/* <div className="audio__progress--wrapper">
+              <div className="audio__time"></div>
+              <input
+                type="range"
+                value="0"
+                max="292.872"
+                className="audio__progress--bar"
+              />
+              <div className="audio__time">time</div>
+            </div> */}
           </div>
         </>
+      ) : (
+        <p>Loading audio data...</p>
       )}
     </div>
   );
