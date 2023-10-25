fc53ee7d422db8f07a42257c1c45194fb2c03fcf
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-10-26 00:09:20 +0100

    plan

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,138 +1,29 @@
-import React, { useState } from 'react';
-import DescriptionIcon from "@mui/icons-material/Description";
-import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
-import HandshakeIcon from "@mui/icons-material/Handshake";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import FAQ from '../components/FAQ'
+import { useState } from "react";
+import { BsChevronCompactDown, BsChevronCompactUp } from "react-icons/bs";
 
-const Accordion = () => {
-const [faqs, setFaqs] = useState([
-    {
-        question: "How does the free 7-day trial work?",
-        answer: "   Begin your complimentary 7-day trial with a Summarist annual membership. You are under no obligation to continue your subscription, and you will only be billed when the trial period expires. With Premium access, you can learn at your own pace and as frequently as you desire, and you may terminate your subscription prior to the conclusion of the 7-day free trial. ",
-        open: true
-    },
-    {
-      question: " Can I switch subscriptions from monthly to yearly, or yearly to monthly?",
-      answer: "   While an annual plan is active, it is not feasible to switch to a monthly plan. However, once the current month ends, transitioning from a monthly plan to an annual plan is an option. ",
-      open: false
-  },
-{
-  question: "What's included in the Premium plan?",
-  answer: " Premium membership provides you with the ultimate Summarist experience, including unrestricted entry to many best-selling books high-quality audio, the ability to download titles for offline reading, and the option to send your reads to your Kindle.",
-  open: false
-},
-{
-  question: "Can I cancel during my trial or subscription?",
-  answer: "You will not be charged if you cancel your trial before its conclusion. While you will not have complete access to the entire Summarist library, you can still expand your knowledge with one curated book per day.",
-  open: false
-},
-])
+export default function Accordion({ title, content }) {
+  const [activeAccordion, setActiveAccordion] = useState(false);
 
-const toggleFAQ = index  => {
-  setFaqs(faqs.map((faq, i) => {
-    if (i === index) {
-      faq.open = !faq.open
-    } else  {
-      faq.open = false
-    }
-    return faq
-  }))
-}
-
-    return (
-        <div>
-                  <div className="faq__wrapper">
-                  {
-  faqs.map((faq, i) => (
-   <div>
-   <FAQ faq={faq} index={i} toggleFAQ={toggleFAQ}/>
-   </div>
-  ))
-}
-                {/* <div className="accordion__card">
-                  <div className="accordion__header">
-                    <div className="accordion__title">
-                      How does the free 7-day trial work?
-                    </div>
-                    <KeyboardArrowUpIcon
-                      style={{ height: "1em", width: "1em" }}
-                      className="accordion__icon"
-                    />
-                  </div>
-
-                  <div style={{ height: "0px" }} className="collapse"></div>
-                  <div  className="accordion__body">
-                    Begin your complimentary 7-day trial with a Summarist annual
-                    membership. You are under no obligation to continue your
-                    subscription, and you will only be billed when the trial
-                    period expires. With Premium access, you can learn at your
-                    own pace and as frequently as you desire, and you may
-                    terminate your subscription prior to the conclusion of the
-                    7-day free trial.
-                  </div>
-                </div>
-                <div className="accordion__card">
-                  <div className="accordion__header">
-                    <div className="accordion__title">
-                    Can I switch subscriptions from monthly to yearly, or yearly to monthly?
-                    </div>
-                    <KeyboardArrowUpIcon
-                      style={{ height: "1em", width: "1em" }}
-                      className="accordion__icon"
-                    />
-                  </div>
-
-                  <div style={{ height: "0px" }} className="collapse"></div>
-                  <div className="accordion__body">
-                    While an annual plan is active, it is not feasible to switch
-                    to a monthly plan. However, once the current month ends,
-                    transitioning from a monthly plan to an annual plan is an
-                    option.
-                  </div>
-                </div>
-                <div className="accordion__card">
-                  <div className="accordion__header">
-                    <div className="accordion__title">
-                    What's included in the Premium plan?
-                    </div>
-                    <KeyboardArrowUpIcon
-                      style={{ height: "1em", width: "1em" }}
-                      className="accordion__icon"
-                    />
-                  </div>
-
-                  <div style={{ height: "0px" }} className="collapse"></div>
-                  <div className="accordion__body">
-                    Premium membership provides you with the ultimate Summarist
-                    experience, including unrestricted entry to many
-                    best-selling books high-quality audio, the ability to
-                    download titles for offline reading, and the option to send
-                    your reads to your Kindle.
-                  </div>
-                </div>
-                <div className="accordion__card">
-                  <div className="accordion__header">
-                    <div className="accordion__title">
-                    Can I cancel during my trial or subscription?
-                    </div>
-                    <KeyboardArrowUpIcon
-                      style={{ height: "1em", width: "1em" }}
-                      className="accordion__icon"
-                    />
-                  </div>
-
-                  <div style={{ height: "0px" }} className="collapse"></div>
-                  <div className="accordion__body">
-                    You will not be charged if you cancel your trial before its
-                    conclusion. While you will not have complete access to the
-                    entire Summarist library, you can still expand your
-                    knowledge with one curated book per day.
-                  </div>
-                </div> */}
-              </div>
+  return (
+    <>
+      <div className="accordion__card">
+        <div
+          className="accordion__header"
+          onClick={() => setActiveAccordion(!activeAccordion)}
+        >
+          <div className="accordion__title">{title}</div>
+          <div>
+            {activeAccordion ? (
+              <BsChevronCompactUp className="accordion__icon" />
+            ) : (
+              <BsChevronCompactDown
+                className="accordion__icon"
+              />
+            )}
+          </div>
         </div>
-    );
+        {activeAccordion && <div className="accordion__body">{content}</div>}
+      </div>
+    </>
+  );
 }
-
-export default Accordion;
--- src/pages/Plan.js
+++ src/pages/Plan.js
@@ -3,189 +3,30 @@ import DescriptionIcon from "@mui/icons-material/Description";
 import LocalFloristIcon from "@mui/icons-material/LocalFlorist";
 import HandshakeIcon from "@mui/icons-material/Handshake";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import Accordion from "../components/Accordion"
+import Accordion from "../components/Accordion";
 const Plan = () => {
-  return (
-    <div>
-      <div className="wrapper wrapper__full">
-        <div className="sidebar__overlay sidebar__overlay--hidden"></div>
-        <div className="plan">
-          <div className="plan__header--wrapper">
-            <div className="plan__header">
-              <div className="plan__title">
-                Get unlimited access to many amazing books to read
-              </div>
-              <div className="plan__sub--title">
-                Turn ordinary moments into amazing learning opportunities
-              </div>
-              <figure className="plan__img--mask">
-                <img
-                  src="https://summarist.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fpricing-top.4d86e93a.png&w=1080&q=75"
-                  className="plan__img"
-                ></img>
-              </figure>
-            </div>
-          </div>
-          <div className="plan__row">
-            <div className="plan__container">
-              <div className="plan__features--wrapper">
-                <div className="plan__features">
-                  <figure className="plan__features--icon">
-                    <DescriptionIcon
-                      style={{ width: "60px", height: "60px" }}
-                    />
-                  </figure>
-                  <div className="plan__features--text">
-                    <b>Key ideas in a few min </b>
-                    with many books to read
-                  </div>
-                </div>
-                <div className="plan__features">
-                  <figure className="plan__features--icon">
-                    <LocalFloristIcon
-                      style={{ width: "60px", height: "60px" }}
-                    />
-                  </figure>
-                  <div className="plan__features--text">
-                    <b>3 million </b>
-                    people growing with Summarist everday
-                  </div>
-                </div>
-                <div className="plan__features">
-                  <figure className="plan__features--icon">
-                    <HandshakeIcon style={{ width: "60px", height: "60px" }} />
-                  </figure>
-                  <div className="plan__features--text">
-                    <b>Precise recommendations </b>
-                    collections curated by experts
-                  </div>
-                </div>
-              </div>
-              <div className="section__title">
-                Choose the plan that fits you
-              </div>
-              <div className="plan__card plan__card--active">
-                <div className="plan__card--circle">
-                  <div className="plan__card--dot"></div>
-                </div>
-                <div className="plan__card--content">
-                  <div className="plan__card--title">Premium Plus Yearly</div>
-                  <div className="plan__card--price">$99.99/year</div>
-                  <div className="plan__card--text">
-                    7-day free trial included
-                  </div>
-                </div>
-              </div>
-              <div className="plan__card--separator">
-                <div className="plan__separator">or</div>
-              </div>
-              <div className="plan__card ">
-                <div className="plan__card--circle"></div>
-                <div className="plan__card--content">
-                  <div className="plan__card--title">Premium Monthly</div>
-                  <div className="plan__card--price">$9.99/month</div>
-                  <div className="plan__card--text">No trial included</div>
-                </div>
-              </div>
-              <div className="plan__card--cta">
-                <span className="btn--wrapper">
-                  <button style={{ width: "300px" }} className="plan__btn">
-                    Start your free 7-day trial
-                  </button>
-                </span>
-                <div className="plan__disclaimer">
-                  Cancel your trial at any time before it ends, and you won’t be
-                  charged.
-                </div>
-              </div>
-    <Accordion/>
-            </div>
-          </div>
-          <section id="footer">
-            <div className="footer__container">
-              <div className="footer__row">
-                <div className="footer__top--wrapper">
-                  <div className="footer__block">
-                    <div className="footer__link--title">Actions</div>
-                    <div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Summarist Magazine</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Cancel Subscription</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Help</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Contact us</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="footer__block">
-                    <div className="footer__link--title">Useful Links</div>
-                    <div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Summarist Magazine</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Cancel Subscription</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Help</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Contact us</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="footer__block">
-                    <div className="footer__link--title">Company</div>
-                    <div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Summarist Magazine</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Cancel Subscription</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Help</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Contact us</a>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="footer__block">
-                    <div className="footer__link--title">Other</div>
-                    <div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Summarist Magazine</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Cancel Subscription</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Help</a>
-                      </div>
-                      <div className="footer__link--wrapper">
-                        <a className="footer__link">Contact us</a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="footer__copyright--wrapper">
-                  <div className="footer__copyright">
-                    Copyright © 2023 Summarist.
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
-        </div>
-      </div>
-    </div>
-  );
-};
+  const accordionData = [
+    {
+      title: "How does the free 7-day trial work?",
+      content:
+        "Begin your complimentary 7-day trial with a Summarist annual membership. You are under no obligation to continue your subscription, and you will only be billed when the trial period expires. With Premium access, you can learn at your own pace and as frequently as you desire, and you may terminate your subscription prior to the conclusion of the 7-day free trial.",
+    },
+    {
+      title:
+        "Can I switch subscriptions from monthly to yearly, or yearly to monthly?",
+      content:
+        "While an annual plan is active, it is not feasible to switch to a monthly plan. However, once the current month ends, transitioning from a monthly plan to an annual plan is an option.",
+    },
+    {
+      title: "What's included in the Premium plan?",
+      content:
+        "Premium membership provides you with the ultimate Summarist experience, including unrestricted entry to many best-selling books high-quality audio, the ability to download titles for offline reading, and the option to send your reads to your Kindle.",
+    },
+    {
+      title: "Can I cancel during my trial or subscription?",
+      content:
+        "You will not be charged if you cancel your trial before its conclusion. While you will not have complete access to the entire Summarist library, you can still expand your knowledge with one curated book per day.",
+    },
+  ];
 
-export default Plan;
+
