5aae1c140990806a1770fd7b442abf1444c1b2b7
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-10-26 01:13:07 +0100

    error

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -12,67 +12,60 @@ import { useRouter } from "next/router";
 
 export default function SideBar() {
   const router = useRouter();
-  const { id, pathname } = router.query;
+  const { pathname } = router.query || {};
+  const isPlayerPage = pathname && pathname.startsWith("/player/");
   const logUserOut = (event) => {
-    event.preventDefault(); // Prevent the default form submission behavior
+    event.preventDefault(); 
     auth.signOut();
     // window.location.reload(); // Commenting this out since it's not necessary
   };
 
-  // className={`sidebar__wrapper ${
-  //   pathname && pathname.startsWith("/player")
-  //     ? "sidebar__bump-up"
-  //     : ""
-  // }`}
-
   return (
-    // <div className="sidebar hidden sm:flex flex-col fixed bg-[#f7faf9] pr-6 z-[1]">
-    <div   className={`sidebar__wrapper ${
-      pathname && pathname.startsWith("/player")
-        ? "sidebar__bump-up"
-        : ""
-    }`}>
+    <div
+      className={`${
+        pathname.startsWith("/player/") ? "sidebar__bump-up" : "sidebar"
+      } hidden sm:flex flex-col fixed bg-[#f7faf9] pr-6 z-[1]`}
+    >
       <div>
         <img
           className="w-[100] h-[40px] ml-5 mt-5"
           src="../assets/summarist.webp"
         />
       </div>
-      <nav className="space-y-5 pl-5">
+      <nav className="space-y-5 pl-5 h-full">
         <div className="group__wrapper">
-              <div className="top__4">
-                 <Link href="/ForYou">
-          <SideBarLink Icon={HomeOutlinedIcon} text={"For you"} />
-        </Link>
+          <div className="top__4">
+            <Link href="/ForYou">
+              <SideBarLink Icon={HomeOutlinedIcon} text={"For you"} />
+            </Link>
 
-        <SideBarLink text={"My Library"} Icon={TurnedInNotOutlinedIcon} />
-        <SideBarLink disabled text={"Highlights"} Icon={EditOutlinedIcon} />
-        <SideBarLink disabled text={"Search"} Icon={SearchOutlinedIcon} />
-        </div>
- 
-
-
-        <div className="space-y-5">
-          <div className="bottom__3">
-                   <Link href="/Settings">
-            <SideBarLink text={"Settings"} Icon={SettingsSuggestOutlinedIcon} />
-          </Link>
-
-          <SideBarLink
-            disabled
-            text={"Help & Support"}
-            Icon={HelpOutlineOutlinedIcon}
-          />
-          <SideBarLink
-            text={"Logout"}
-            Icon={LogoutOutlinedIcon}
-            onClick={(e) => logUserOut(e)}
-            className="logout-btn"
-          />
+            <SideBarLink text={"My Library"} Icon={TurnedInNotOutlinedIcon} />
+            <SideBarLink disabled text={"Highlights"} Icon={EditOutlinedIcon} />
+            <SideBarLink disabled text={"Search"} Icon={SearchOutlinedIcon} />
           </div>
-        </div>
-    
-   
+
+          <div className="space-y-5">
+            <div className="bottom__3">
+              <Link href="/Settings">
+                <SideBarLink
+                  text={"Settings"}
+                  Icon={SettingsSuggestOutlinedIcon}
+                />
+              </Link>
+
+              <SideBarLink
+                disabled
+                text={"Help & Support"}
+                Icon={HelpOutlineOutlinedIcon}
+              />
+              <SideBarLink
+                text={"Logout"}
+                Icon={LogoutOutlinedIcon}
+                onClick={(e) => logUserOut(e)}
+                className="logout-btn"
+              />
+            </div>
+          </div>
         </div>
       </nav>
     </div>
@@ -84,7 +77,7 @@ function SideBarLink({ text, Icon, disabled, onClick, className }) {
   return (
     <li
       onClick={onClick} // Handle click event here
-      className={`hoverAnimation mb-2.5 flex items-center text-[16px] list-none mt-14 ml-2 ${linkClass} ${className}`}
+      className={`hoverAnimation mb-14 flex items-center text-[16px] list-none mt-14 ml-2 ${linkClass} ${className}`}
     >
       <Icon className="mr-4" />
       {text}
