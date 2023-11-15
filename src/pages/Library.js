545ca30ea1705bcf49c54e91911e0ca4194edceb
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-11-15 00:40:58 +0000

    library

--- src/pages/Library.js
+++ src/pages/Library.js
@@ -1,3 +1,4 @@
+// Import necessary libraries and components
 import React, { useEffect, useState } from "react";
 import SideBar from "../components/SideBar";
 import Input from "../components/Input";
@@ -6,13 +7,30 @@ import { auth, db } from "../../firebase";
 import axios from "axios";
 import StarBorderIcon from "@mui/icons-material/StarBorder";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import { useDispatch, useSelector } from "react-redux";
+import { initializeAuth, setUser } from "../redux/userSlice";
 
 const Library = () => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user); // Assuming you have a user slice in your Redux store
   const [savedBooks, setSavedBooks] = useState([]);
+  const [isUserAuth, setIsUserAuth] = useState(false);
 
   useEffect(() => {
-    const user = auth.currentUser;
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(setUser(user)); // Dispatch user details to Redux store
+        dispatch(initializeAuth());
+        setIsUserAuth(true);
+      } else {
+        setIsUserAuth(false);
+      }
+    });
 
+    return () => unsubscribe();
+  }, [dispatch]);
+
+  useEffect(() => {
     const fetchSavedBooks = async () => {
       try {
         const librarySnapshot = await getDocs(
@@ -23,10 +41,8 @@ const Library = () => {
           ...doc.data(),
         }));
 
-        // Extract book IDs
         const bookIds = books.map((book) => book.bookId);
 
-        // Use the book IDs to make API calls for each book
         const apiCalls = bookIds.map(async (bookId) => {
           const { data } = await axios.get(
             `https://us-central1-summaristt.cloudfunctions.net/getBook?id=${bookId}`
@@ -34,55 +50,54 @@ const Library = () => {
           return data;
         });
 
-        // Wait for all API calls to complete
         const bookDetails = await Promise.all(apiCalls);
-
         setSavedBooks(bookDetails);
       } catch (error) {
         console.error("Error fetching saved books:", error);
       }
     };
 
-    if (user) {
+    if (isUserAuth && user) {
       fetchSavedBooks();
     }
-  }, []);
+  }, [isUserAuth, user]);
+
+  if (!isUserAuth) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
       <SideBar />
       <Input />
+      <div className="Saved__books--title">Saved Books</div>
       <div className="library-container">
-        <div className="for-you__title">Saved Books</div>
-
         {savedBooks.map((book) => (
           <div key={book.id}>
             <div className="library__saved-books">
-              <div className="library__books-link ">
-                <div className="library__book_image--wrapper ">
-                  <img className="library__book-img" src={book.imageLink} />
-                  </div>
-
-                  <div className="saved__book--title">{book.title}</div>
-                  <div className="saved__book--author">{book.author}</div>
-                  <div className="saved__book--sub-title">{book.subTitle}</div>
-                  <div className="library__details--wrapper">
-                    <div className="library__book--details ">
-                      <div className="library__book--details-icon">
-                        <AccessTimeIcon />
-                      </div>
-                      <div className="library__book--details-text"></div>
+              <div className="library__books-link">
+                <div className="library__book_image--wrapper">
+                  <img className="library__book-img" src={book.imageLink} alt={book.title} />
+                </div>
+                <div className="saved__book--title">{book.title}</div>
+                <div className="saved__book--author">{book.author}</div>
+                <div className="saved__book--sub-title">{book.subTitle}</div>
+                <div className="library__details--wrapper">
+                  <div className="library__book--details">
+                    <div className="library__book--details-icon">
+                      <AccessTimeIcon style={{ height: '100%', width: '100%' }} />
                     </div>
-                    <div className="library__book--details ">
-                      <div className="library__book--details-icon">
-                        <StarBorderIcon />
-                      </div>
-                      <div className="library__book--details-text">
-                        {book.averageRating}
-                      </div>
+                    <div className="library__book--details-text"></div>
+                  </div>
+                  <div className="library__book--details">
+                    <div className="library__book--details-icon">
+                      <StarBorderIcon style={{ height: '100%', width: '100%' }} />
+                    </div>
+                    <div className="library__book--details-text">
+                      {book.averageRating}
                     </div>
                   </div>
-            
+                </div>
               </div>
             </div>
           </div>
