606bce11164f2f87a63542b1535f460439bce27b
S-J-K123 <98359890+S-J-K123@users.noreply.github.com>
2023-12-01 17:10:59 +0000

    book mark

---
+++ src/components/BookDetailsSkeleton.js
@@ -0,0 +1,28 @@
+// // import React from "react";
+// import Skeleton from "./Skeleton";
+
+// const BookDetailsSkeleton = () => {
+//   const skeletonItems = [
+//     // { key: "title", width: 400, height: 50 },
+//     // { key: "author", width: 400, height: 20 },
+//     // { key: "subtitle", width: 400, height: 20 },
+//     // { key: "averageRating", width: 24, height: 24 },
+//     { key: "authorDescription", width: 120, height: 24 },
+//     // Add more items as needed
+//   ];
+
+//   return (
+//     <div>
+//       {skeletonItems.map((item) => (
+//         <div key={item.key} className="inner-book-description">
+//           <div className="inner-book-icon">
+//             <Skeleton width={item.width} height={item.height} />
+//           </div>
+//           <Skeleton width={item.width} height={item.height} />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default BookDetailsSkeleton;
